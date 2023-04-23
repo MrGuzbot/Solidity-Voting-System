0de54252a6e2b17176439d15c55725f1e59623d3
Gustavo Rodrigues <121345685+MrGuzbot@users.noreply.github.com>
2023-04-23 04:15:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 // Set up Alchemy Web3 provider
-const web3Provider = new Web3.providers.HttpProvider("https://eth-mainnet.alchemyapi.io/v2/<your-alchemy-api-key>");
-
-// Set up Web3 with Alchemy provider
-const web3 = new Web3(1Pqv4pU2WcJFStiwRdNSS6y6me-bhAnC);
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
